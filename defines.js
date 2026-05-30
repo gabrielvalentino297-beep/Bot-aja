55b93e181aa7c58b5332afea8cdf9265a8df3a5c
gabrielvalentino297-beep <gabrielvalentino297@gmail.com>
2026-05-30 16:51:15 +0700

    Create defines.js

---
+++ defines.js
@@ -0,0 +1,21 @@
+module.exports = {
+    client: {
+        token: "MTQwMTkwMDg2MDc3MDA5MTA2OA.Gwdbn2.MLEF7wOki83fZNXaoES5owp6n8JFmUMcyDqRBk",
+        id: "1401900860770091068",
+        guild: "1506632247342796922",
+        version: "1.0",
+    },
+    icon: {
+        thumbnail: "https://media.discordapp.net/attachments/1503767759182631022/1509428182418329640/IMG-20260525-WA02871.jpg?ex=6a19240a&is=6a17d28a&hm=77f5309527d1afe15af1b0109cb747ffabb617f79f5ae7846aa1b3acc4257e7e&=&format=webp&width=607&height=607",
+    },
+    handler: {
+        prefix: "!",
+    },
+    users: {
+        ownerId: "1465995715896676394",
+        developers: ["1465995715896676394"],
+    },
+    servers: {
+        name: "DRAMAMINE ROLEPLAY",
+    },
+};
