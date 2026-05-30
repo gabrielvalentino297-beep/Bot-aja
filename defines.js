0af11fe0616a486e0dad0907146e1c41d4dd273e
gabrielvalentino297-beep <gabrielvalentino297@gmail.com>
2026-05-30 17:30:39 +0700

    Update defines.js

--- defines.js
+++ defines.js
@@ -1,6 +1,6 @@
 module.exports = {
     client: {
-        token: process.env.TOKEN,
+        token: "MTUwOTE0MjAzMzc5OTk3NDk4Mg.GeMZwP.GZ2z-Qgf8c7_nA3_GYCJFUgweEczXacRbG03ZA", 
         id: "1401900860770091068",
         guild: "1506632247342796922",
         version: "1.0",
